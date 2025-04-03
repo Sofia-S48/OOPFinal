fb4fa316191b858ba974b93f1c1796f561c5da01
Sofia-S48 <sofia.saldumbide@gmail.com>
2025-04-03 10:16:11 -0400

    slight modification

--- Program.cs
+++ Program.cs
@@ -7,10 +7,7 @@ class Program
 {
     static void Main()
     {
-        VehcileManager manager = new VehcileManager();
-
-        Car myCar = new Car("Chevrolet", 350, 50000, "Cruze", 250);
-        Console.AddVehicle(myCar);
+        
 
         
     }
